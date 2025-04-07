48d9ed160fb886c43484e2e8cca814660a49aebd
kemalgencay <kemalgencay@gmail.com>
2025-04-07 10:25:33 +0300

    third post

--- routes/index.tsx
+++ routes/index.tsx
@@ -7,19 +7,17 @@ export const handler: Handlers<Post[]> = {
     return ctx.render(posts);
   },
 };
-
 export default function BlogIndexPage(props: PageProps<Post[]>) {
   const posts = props.data;
   return (
     <main class="max-w-screen-md px-4 pt-16 mx-auto">
       <h1 class="text-5xl font-bold">Blog</h1>
       <div class="mt-8">
-        {posts.map((post) => <PostCard key={post.slug} post={post} />)}
+        {posts.map((post) => <PostCard post={post} />)}
       </div>
     </main>
   );
 }
-
 function PostCard(props: { post: Post }) {
   const { post } = props;
   return (
