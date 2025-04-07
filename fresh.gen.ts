97076bd0885a866825db87844ae8d1ea857b9192
kemalgencay <kemalgencay@gmail.com>
2025-04-07 11:32:56 +0300

    the last straw!

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -2,18 +2,18 @@
 // This file SHOULD be checked into source version control.
 // This file is automatically updated during development when running `dev.ts`.
 
+import * as $_slug_ from "./routes/[slug].tsx";
 import * as $_404 from "./routes/_404.tsx";
 import * as $_app from "./routes/_app.tsx";
-import * as $greet_slug_ from "./routes/greet/[slug].tsx";
 import * as $index from "./routes/index.tsx";
 
 import type { Manifest } from "$fresh/server.ts";
 
 const manifest = {
   routes: {
+    "./routes/[slug].tsx": $_slug_,
     "./routes/_404.tsx": $_404,
     "./routes/_app.tsx": $_app,
-    "./routes/greet/[slug].tsx": $greet_slug_,
     "./routes/index.tsx": $index,
   },
   islands: {},
