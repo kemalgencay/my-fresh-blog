7f9c738180c466f1949b0465553298dcc4f50e94
kemalgencay <kemalgencay@gmail.com>
2025-04-07 08:23:01 +0300

    one more commit3

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,5 +1,5 @@
 import { Handlers, PageProps } from "$fresh/server.ts";
-import { getPost as _getPost, getPosts, Post } from "../utils/posts.ts";
+import { getPost, getPosts, Post } from "../utils/posts.ts";
 
 export const handler: Handlers<Post[]> = {
   async GET(_req, ctx) {
