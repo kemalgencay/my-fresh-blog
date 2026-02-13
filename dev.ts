2c1f1f9e9ca1442fa9bcb648a92ee2810b70bf3f
Kemal Gencay <kemalgencay@gmail.com>
2026-02-13 09:28:32 +0300

    forth commit

--- dev.ts
+++ dev.ts
@@ -3,6 +3,8 @@
 import dev from "$fresh/dev.ts";
 import config from "@/fresh.config.ts";
 
-import "@std/dotenv/load";
+import {load} from "jsr:@std/dotenv@^0.225.6";
 
+
+await load({export: true});
 await dev(import.meta.url, "./main.ts", config);
